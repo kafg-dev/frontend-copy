265fd4cc9587c3354b492c0ff598704314ae910e
lashween <lashweenraj2806@gmail.com>
2021-08-09 14:41:13 +0800

    Updated footer

--- src/pages/auth/reset/Reset.js
+++ src/pages/auth/reset/Reset.js
@@ -1,37 +1,37 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Link, withRouter } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { Alert, Button, Container } from 'reactstrap';
-import Widget from '../../../components/Widget';
-import { authError, resetPassword } from '../../../actions/auth';
+import React from "react";
+import PropTypes from "prop-types";
+import { Link, withRouter } from "react-router-dom";
+import { connect } from "react-redux";
+import { Alert, Button, Container } from "reactstrap";
+import Widget from "../../../components/Widget";
+import { authError, resetPassword } from "../../../actions/auth";
 
 class Reset extends React.Component {
-    static propTypes = {
-        dispatch: PropTypes.func.isRequired,
+  static propTypes = {
+    dispatch: PropTypes.func.isRequired,
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      password: "",
+      confirmPassword: "",
     };
 
-    constructor(props) {
-        super(props);
-
-      this.state = {
-        password: '',
-        confirmPassword: ''
-      };
-
-        this.changePassword = this.changePassword.bind(this);
-        this.changeConfirmPassword = this.changeConfirmPassword.bind(this);
-        this.checkPassword = this.checkPassword.bind(this);
-        this.isPasswordValid = this.isPasswordValid.bind(this);
-        this.doReset = this.doReset.bind(this);
-    }
+    this.changePassword = this.changePassword.bind(this);
+    this.changeConfirmPassword = this.changeConfirmPassword.bind(this);
+    this.checkPassword = this.checkPassword.bind(this);
+    this.isPasswordValid = this.isPasswordValid.bind(this);
+    this.doReset = this.doReset.bind(this);
+  }
 
   changePassword(event) {
-    this.setState({password: event.target.value});
+    this.setState({ password: event.target.value });
   }
 
   changeConfirmPassword(event) {
-    this.setState({confirmPassword: event.target.value});
+    this.setState({ confirmPassword: event.target.value });
   }
 
   checkPassword() {
@@ -43,21 +43,23 @@ class Reset extends React.Component {
       }
       setTimeout(() => {
         this.props.dispatch(authError());
-      }, 3 * 1000)
+      }, 3 * 1000);
     }
   }
 
   isPasswordValid() {
-    return this.state.password && this.state.password === this.state.confirmPassword;
+    return (
+      this.state.password && this.state.password === this.state.confirmPassword
+    );
   }
 
   doReset(e) {
     e.preventDefault();
 
     const params = new URLSearchParams(this.props.location.search);
-    const token = params.get('token');
+    const token = params.get("token");
     if (!token) {
-      authError("There are no token")
+      authError("There are no token");
     }
 
     if (!this.isPasswordValid()) {
@@ -67,53 +69,77 @@ class Reset extends React.Component {
     }
   }
 
-    render() {
-      return (
-        <div className="auth-page">
-          <Container>
-            <h5 className="auth-logo">
-              <i className="la la-circle text-gray"/>
-              Sing App React
-              <i className="la la-circle text-warning"/>
-            </h5>
-            <Widget className="widget-auth mx-auto" title={<h3 className="mt-0">Reset password</h3>}>
-              <p className="widget-auth-info">
-                Please fill all fields below
-              </p>
-              <form className="mt" onSubmit={this.doReset}>
-                {
-                  this.props.errorMessage && (
-                    <Alert className="alert-sm" color="danger">
-                      {this.props.errorMessage}
-                    </Alert>
-                  )
-                }
-                <div className="form-group">
-                  <input className="form-control no-border" value={this.state.password}
-                         onChange={this.changePassword} type="password" required name="password"
-                         placeholder="Password"/>
-                </div>
-                <div className="form-group">
-                  <input className="form-control no-border" value={this.state.confirmPassword}
-                         onChange={this.changeConfirmPassword} onBlur={this.checkPassword} type="password" required
-                         name="confirmPassword"
-                         placeholder="Confirm"/>
-                </div>
-                <Button type="submit" color="inverse" className="auth-btn mb-3"
-                        size="sm">{this.props.isFetching ? 'Loading...' : 'Reset'}</Button>
-              </form>
-              <p className="widget-auth-info">
-                or
-              </p>
-              <Link className="d-block text-center" to="login">Enter the account</Link>
-            </Widget>
-          </Container>
-          <footer className="auth-footer">
-            {new Date().getFullYear()} &copy; Sing App - React Admin Dashboard Template. By <a rel="noopener noreferrer" target="_blank" href="https://flatlogic.com">Flatlogic</a>
-          </footer>
-        </div>
-      );
-    }
+  render() {
+    return (
+      <div className="auth-page">
+        <Container>
+          <h5 className="auth-logo">
+            <i className="la la-circle text-gray" />
+            Sing App React
+            <i className="la la-circle text-warning" />
+          </h5>
+          <Widget
+            className="widget-auth mx-auto"
+            title={<h3 className="mt-0">Reset password</h3>}
+          >
+            <p className="widget-auth-info">Please fill all fields below</p>
+            <form className="mt" onSubmit={this.doReset}>
+              {this.props.errorMessage && (
+                <Alert className="alert-sm" color="danger">
+                  {this.props.errorMessage}
+                </Alert>
+              )}
+              <div className="form-group">
+                <input
+                  className="form-control no-border"
+                  value={this.state.password}
+                  onChange={this.changePassword}
+                  type="password"
+                  required
+                  name="password"
+                  placeholder="Password"
+                />
+              </div>
+              <div className="form-group">
+                <input
+                  className="form-control no-border"
+                  value={this.state.confirmPassword}
+                  onChange={this.changeConfirmPassword}
+                  onBlur={this.checkPassword}
+                  type="password"
+                  required
+                  name="confirmPassword"
+                  placeholder="Confirm"
+                />
+              </div>
+              <Button
+                type="submit"
+                color="inverse"
+                className="auth-btn mb-3"
+                size="sm"
+              >
+                {this.props.isFetching ? "Loading..." : "Reset"}
+              </Button>
+            </form>
+            <p className="widget-auth-info">or</p>
+            <Link className="d-block text-center" to="login">
+              Enter the account
+            </Link>
+          </Widget>
+        </Container>
+        <footer className="auth-footer">
+          {new Date().getFullYear()} &copy; Vodevi Portal. By{" "}
+          <a
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://ronasnetwork.com"
+          >
+            Ronas Network
+          </a>
+        </footer>
+      </div>
+    );
+  }
 }
 
 function mapStateToProps(state) {
@@ -124,4 +150,3 @@ function mapStateToProps(state) {
 }
 
 export default withRouter(connect(mapStateToProps)(Reset));
-
