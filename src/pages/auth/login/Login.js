73a7296d958a98b66e8f2bf10d2a6bb6709e8311
Lashween <lashweenraj2806@gmail.com>
2021-08-13 08:19:01 +0800

    Added signup button to login page

--- src/pages/auth/login/Login.js
+++ src/pages/auth/login/Login.js
@@ -7,9 +7,6 @@ import { loginUser, receiveToken, doInit } from "../../../actions/auth";
 import jwt from "jsonwebtoken";
 import { push } from "connected-react-router";
 import classnames from "classnames";
-
-// styles
-// import UseStyles from "./styles";
 import { withStyles } from "@material-ui/core/styles";
 import {
   Grid,
@@ -21,156 +18,7 @@ import {
   TextField,
   Fade,
 } from "@material-ui/core";
-import google from "../../images/google.svg";
-
-const styles = (theme) => ({
-  container: {
-    height: "100vh",
-    width: "100vw",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    position: "absolute",
-    top: 0,
-    left: 0,
-  },
-  logotypeContainer: {
-    backgroundImage: `url(${
-      process.env.PUBLIC_URL + "/images/login/background.jpg"
-    })`,
-    backgroundSize: "cover",
-    width: "60%",
-    height: "100%",
-    display: "flex",
-    justifyContent: "flex-end",
-    alignItems: "flex-end",
-    [theme.breakpoints.down("sm")]: {
-      width: "50%",
-    },
-    [theme.breakpoints.down("sm")]: {
-      display: "none",
-    },
-  },
-  logotypeImage: {
-    width: "42.5%",
-    marginBottom: "3.5%",
-    marginRight: "2.5%",
-  },
-  logotypeText: {
-    color: "white",
-    fontWeight: 500,
-    fontSize: 40,
-    fontFamily: "Lucida Console",
-    textShadow: "0 0 12px #abcbe0, 0 0 12px #abcbe0",
-    [theme.breakpoints.down("sm")]: {
-      fontSize: 48,
-    },
-  },
-  formContainer: {
-    width: "40%",
-    height: "100%",
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
-    [theme.breakpoints.down("sm")]: {
-      width: "50%",
-    },
-  },
-  form: {
-    width: 320,
-  },
-  tab: {
-    fontWeight: 400,
-    fontSize: 18,
-  },
-  greeting: {
-    fontSize: 30,
-    fontWeight: 500,
-    textAlign: "center",
-    marginTop: theme.spacing(2),
-  },
-  googleButton: {
-    marginTop: theme.spacing(6),
-    // boxShadow: theme.customShadows.widget,
-    backgroundColor: "white",
-    width: "100%",
-    textTransform: "none",
-  },
-  googleButtonCreating: {
-    marginTop: 0,
-  },
-  googleIcon: {
-    width: 30,
-    marginRight: theme.spacing(2),
-  },
-  creatingButtonContainer: {
-    marginTop: theme.spacing(2.5),
-    height: 46,
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  createAccountButton: {
-    height: 46,
-    textTransform: "none",
-  },
-  formDividerContainer: {
-    marginTop: theme.spacing(4),
-    marginBottom: theme.spacing(4),
-    display: "flex",
-    alignItems: "center",
-  },
-  formDividerWord: {
-    paddingLeft: theme.spacing(2),
-    paddingRight: theme.spacing(2),
-  },
-  formDivider: {
-    flexGrow: 1,
-    height: 1,
-    backgroundColor: theme.palette.text.hint + "40",
-  },
-  errorMessage: {
-    textAlign: "center",
-  },
-  textFieldUnderline: {
-    "&:before": {
-      borderBottomColor: theme.palette.primary.light,
-    },
-    "&:after": {
-      borderBottomColor: theme.palette.primary.main,
-    },
-    "&:hover:before": {
-      borderBottomColor: `${theme.palette.primary.light} !important`,
-    },
-  },
-  textField: {
-    borderBottomColor: theme.palette.background.light,
-  },
-  formButtons: {
-    width: "100%",
-    marginTop: theme.spacing(4),
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "center",
-  },
-  forgetButton: {
-    textTransform: "none",
-    fontWeight: 400,
-  },
-  loginLoader: {
-    marginLeft: theme.spacing(4),
-  },
-  copyright: {
-    marginTop: theme.spacing(4),
-    whiteSpace: "nowrap",
-    [theme.breakpoints.up("sm")]: {
-      position: "absolute",
-      bottom: theme.spacing(2),
-    },
-  },
-});
-
+import { styles } from "./styles";
 class Login extends React.Component {
   static propTypes = {
     dispatch: PropTypes.func.isRequired,
@@ -193,13 +41,10 @@ class Login extends React.Component {
       email: "",
       password: "",
       isLoading: false,
-      activeTabId: 0,
       error: null,
     };
 
     this.doLogin = this.doLogin.bind(this);
-    this.googleLogin = this.googleLogin.bind(this);
-    this.microsoftLogin = this.microsoftLogin.bind(this);
     this.changeEmail = this.changeEmail.bind(this);
     this.changePassword = this.changePassword.bind(this);
     this.signUp = this.signUp.bind(this);
@@ -218,14 +63,6 @@ class Login extends React.Component {
     this.props.dispatch(
       loginUser({ email: this.state.email, password: this.state.password })
     );
-  }
-
-  googleLogin() {
-    this.props.dispatch(loginUser({ social: "google" }));
-  }
-
-  microsoftLogin() {
-    this.props.dispatch(loginUser({ social: "microsoft" }));
   }
 
   componentDidMount() {
@@ -258,196 +95,71 @@ class Login extends React.Component {
         </div>
         <div className={classes.formContainer}>
           <div className={classes.form}>
-            <Tabs
-              value={this.state.activeTabId}
-              onChange={(e, id) => this.setState({ activeTabId: id })}
-              indicatorColor="primary"
-              textColor="primary"
-              centered
-            >
-              <Tab label="Login" classes={{ root: classes.tab }} id="1" />
-              <Tab label="New User" classes={{ root: classes.tab }} id="0" />
-            </Tabs>
-            {this.state.activeTabId === 0 && (
-              <React.Fragment>
-                <Fade in={this.state.error}>
-                  <Typography
-                    color="secondary"
-                    className={classes.errorMessage}
-                  >
-                    Something is wrong with your login or password :(
-                  </Typography>
-                </Fade>
-                <TextField
-                  id="email"
-                  InputProps={{
-                    classes: {
-                      underline: classes.textFieldUnderline,
-                      input: classes.textField,
-                    },
-                  }}
-                  value={this.state.email}
-                  onChange={this.changeEmail}
-                  margin="normal"
-                  placeholder="Email Adress"
-                  type="email"
-                  fullWidth
-                />
-                <TextField
-                  id="password"
-                  InputProps={{
-                    classes: {
-                      underline: classes.textFieldUnderline,
-                      input: classes.textField,
-                    },
-                  }}
-                  value={this.state.password}
-                  onChange={this.changePassword}
-                  margin="normal"
-                  placeholder="Password"
-                  type="password"
-                  fullWidth
-                />
-                <div className={classes.formButtons}>
-                  {this.state.isLoading ? (
-                    <CircularProgress
-                      size={26}
-                      className={classes.loginLoader}
-                    />
-                  ) : (
-                    <Button
-                      disabled={
-                        this.state.email.length === 0 ||
-                        this.state.password.length === 0
-                      }
-                      onClick={this.doLogin}
-                      variant="contained"
-                      color="primary"
-                      size="large"
-                    >
-                      Login
-                    </Button>
-                  )}
-                  <Button
-                    color="primary"
-                    size="large"
-                    className={classes.forgetButton}
-                  >
-                    Forget Password
-                  </Button>
-                </div>
-              </React.Fragment>
-            )}
-            {this.state.activeTabId === 1 && (
-              <React.Fragment>
-                <Typography variant="h2" className={classes.greeting}>
-                  Create your account
+            <React.Fragment>
+              <Fade in={this.state.error}>
+                <Typography color="secondary" className={classes.errorMessage}>
+                  Something is wrong with your login or password :(
                 </Typography>
-                <Fade in={this.state.error}>
-                  <Typography
+              </Fade>
+              <TextField
+                id="email"
+                InputProps={{
+                  classes: {
+                    underline: classes.textFieldUnderline,
+                    input: classes.textField,
+                  },
+                }}
+                value={this.state.email}
+                onChange={this.changeEmail}
+                margin="normal"
+                placeholder="Email Address"
+                type="email"
+                fullWidth
+              />
+              <TextField
+                id="password"
+                InputProps={{
+                  classes: {
+                    underline: classes.textFieldUnderline,
+                    input: classes.textField,
+                  },
+                }}
+                value={this.state.password}
+                onChange={this.changePassword}
+                margin="normal"
+                placeholder="Password"
+                type="password"
+                fullWidth
+              />
+              <div className={classes.formButtons}>
+                {this.state.isLoading ? (
+                  <CircularProgress size={26} className={classes.loginLoader} />
+                ) : (
+                  <Button
+                    disabled={
+                      this.state.email.length === 0 ||
+                      this.state.password.length === 0
+                    }
+                    onClick={this.doLogin}
+                    variant="contained"
                     color="secondary"
-                    className={classes.errorMessage}
+                    size="large"
                   >
-                    Something is wrong with your login or password :(
-                  </Typography>
-                </Fade>
-                <TextField
-                  id="name"
-                  InputProps={{
-                    classes: {
-                      underline: classes.textFieldUnderline,
-                      input: classes.textField,
-                    },
-                  }}
-                  // value={this.state.email}
-                  // onChange={e => setNameValue(e.target.value)}
-                  margin="normal"
-                  placeholder="Full Name"
-                  type="text"
-                  fullWidth
-                />
-                <TextField
-                  id="email"
-                  InputProps={{
-                    classes: {
-                      underline: classes.textFieldUnderline,
-                      input: classes.textField,
-                    },
-                  }}
-                  value={this.state.email}
-                  onChange={this.state.changeEmail}
-                  margin="normal"
-                  placeholder="Email Adress"
-                  type="email"
-                  fullWidth
-                />
-                <TextField
-                  id="password"
-                  InputProps={{
-                    classes: {
-                      underline: classes.textFieldUnderline,
-                      input: classes.textField,
-                    },
-                  }}
-                  value={this.state.password}
-                  onChange={this.state.changePassword}
-                  margin="normal"
-                  placeholder="Password"
-                  type="password"
-                  fullWidth
-                />
-                <div className={classes.creatingButtonContainer}>
-                  {this.state.isLoading ? (
-                    <CircularProgress size={26} />
-                  ) : (
-                    <Button
-                      onClick={
-                        () => loginUser()
-                        // userDispatch,
-                        // loginValue,
-                        // passwordValue,
-                        // props.history,
-                        // setIsLoading,
-                        // setError,
-                      }
-                      disabled={
-                        this.state.email.length === 0 ||
-                        this.state.password.length === 0
-                        // nameValue.length === 0
-                      }
-                      size="large"
-                      variant="contained"
-                      color="primary"
-                      fullWidth
-                      className={classes.createAccountButton}
-                    >
-                      Create your account
-                    </Button>
-                  )}
-                </div>
-                <div className={classes.formDividerContainer}>
-                  <div className={classes.formDivider} />
-                  <Typography className={classes.formDividerWord}>
-                    or
-                  </Typography>
-                  <div className={classes.formDivider} />
-                </div>
-                <Button
-                  size="large"
-                  className={classnames(
-                    classes.googleButton,
-                    classes.googleButtonCreating
-                  )}
-                >
-                  <img
-                    src={google}
-                    alt="google"
-                    className={classes.googleIcon}
-                  />
-                  &nbsp;Sign in with Google
+                    Login
+                  </Button>
+                )}
+                <Button onClick={this.signUp} color="primary" size="large">
+                  Sign Up
                 </Button>
-              </React.Fragment>
-            )}
+                {/*<Button
+                  color="primary"
+                  size="large"
+                  className={classes.forgetButton}
+                >
+                  Forget Password
+                </Button>*/}
+              </div>
+            </React.Fragment>
           </div>
           <Typography
             color="primary"
